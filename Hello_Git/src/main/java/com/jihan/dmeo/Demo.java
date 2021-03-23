f4a7de6287d34c01fec59f34c45e8da34961d8dc
together <2629192232@qq.com>
2021-03-23 15:42:37 +0800

    第一次提交

---
+++ Hello_Git/src/main/java/com/jihan/dmeo/Demo.java
@@ -0,0 +1,13 @@
+package com.jihan.dmeo;
+
+/**
+ * @Auther: jihan
+ * @Date: 2021/3/23
+ * @Description: com.jihan.dmeo
+ * @version: 1.0
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("第一次添加");
+    }
+}
